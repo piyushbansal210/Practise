00b09f048fa9854c9713b015070562451d21762a
piyushbansal210 <piyushbansal210@gmail.com>
2022-12-19 00:05:21 +0530

    Binary Tree

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tree tr = new Tree();
+        tr.insert(7);
+        tr.insert(4);
+        tr.insert(9);
+        tr.insert(1);
+        tr.insert(6);
+        tr.insert(8);
+        tr.insert(10);
+        
+        Tree tr2 = new Tree();
+        tr2.insert(7);
+        tr2.insert(4);
+        tr2.insert(9);
+        tr2.insert(1);
+        tr2.insert(6);
+        tr2.insert(8);
+        tr2.insert(10);
+
+        System.out.println(tr.equals(tr2));
     }
 }
---
+++ src/Tree.java
@@ -0,0 +1,136 @@
+public class Tree{
+
+    private class Node{
+        private Node leftChild;
+        private Node rightChild;
+        private int value;
+        private Node(int value){
+            this.value=value;
+        }
+    }
+
+    public Node root;
+
+    public void value(Node node){
+        System.out.println(node.value);
+    }
+
+    public void insert(int n){
+        Node node = new Node(n);
+        if(root==null){
+            root = node;
+            return;
+        }
+        var current=root;
+        while(true){
+            if(n<current.value){
+                if(current.leftChild==null){
+                    current.leftChild=node;
+                    break;                
+                }
+                current=current.leftChild;
+            }
+            else{
+                if(current.rightChild==null){
+                    current.rightChild=node;
+                    break;
+                }
+                current=current.rightChild;
+            }
+        }
+    }
+    public boolean find(int n){
+        var current=root;
+        while(current!=null){
+            if(n<current.value){
+                current=current.leftChild;
+            }
+            else if(n>current.value){
+                current=current.rightChild;
+            }
+            else
+                return true;
+        }
+        return false;
+    }
+    
+    //Level Order Traversal BFS
+    //Scans all the nodes on the same level
+    public void BFS(int val){
+
+    }
+
+    public void PreOrder(){
+        PreOrder(root);
+    }
+
+    public void PostOrder(){
+        PostOrder(root);
+    }
+
+    public void InOrder(){
+        InOrder(root);
+    }
+
+    //Root,Left,Right DFS
+    public void PreOrder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.value+" ");
+        PreOrder(root.leftChild);
+        PreOrder(root.rightChild);
+    }
+
+    //Left,Right, Root DFS
+    public void PostOrder(Node root){
+        if(root==null){
+            return;
+        }
+        PostOrder(root.leftChild);
+        PostOrder(root.rightChild);
+        System.out.print(root.value+" ");
+    }
+
+    //Left, Root, Right  DFS
+    public void InOrder(Node root){
+        if(root==null){
+            return;
+        }
+        InOrder(root.leftChild);
+        System.out.print(root.value+" ");
+        InOrder(root.rightChild);
+    }
+
+    public int height(Node root){
+        if(root.leftChild==null && root.rightChild==null) return 0;
+        return 1+Math.max(
+            height(root.leftChild),
+            height(root.rightChild)
+        );
+    }
+    public int min(Node root){
+        if(root.leftChild==null && root.rightChild==null) return 0;
+
+        var left = min(root.leftChild);
+        var right = min(root.rightChild);
+
+        return Math.min(Math.min(left,right),root.value);
+    }
+
+    public boolean equals(Tree other){
+        return equals(root, other.root);
+    }
+    public boolean equals(Node first, Node second){
+        if(first==null && second==null){
+            return true;
+        }
+        if(first!=null && second!=null){
+            return first.value == second.value
+                    && equals(first.leftChild , second.leftChild)
+                    && equals(first.rightChild , second.rightChild);
+        }
+        return false;
+    }
+
+}
